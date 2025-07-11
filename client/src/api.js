1cf32d6ce67e7fd4bc661839135a6d87185679bc
mohamed-88 <mohamederdni4@gmail.com>
2025-07-11 14:44:32 +0300

    Awayê herî baş ji bo birêvebirina herdu jîngehan

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
+// Awayê herî baş ji bo birêvebirina herdu jîngehan
+let baseURL;
+
+if (process.env.NODE_ENV === 'production') {
+  // Dema li ser Vercel e
+  baseURL = process.env.REACT_APP_API_URL;
+} else {
+  // Dema li ser komputerê ye (localhost)
+  baseURL = 'http://localhost:5000/api';
+}
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+  baseURL: baseURL
 });
 
 export default api;
